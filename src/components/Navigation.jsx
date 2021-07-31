54fbbdc568e1deabb8d38d592991341d55cd7a9b
Anu Shibin <anushibin007@gmail.com>
2021-07-31 23:22:22 +0530

    Close Nav on Link click

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,26 +1,36 @@
 import "../App.css";
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 function Navigation() {
+	const [expanded, setExpanded] = useState(false);
+
+	const navToggle = () => {
+		setExpanded(expanded ? false : true);
+	};
+
+	const closeNav = () => {
+		setExpanded(false);
+	};
+
 	return (
 		<React.Fragment>
-			<Navbar bg="primary" variant="dark" expand="lg">
+			<Navbar bg="primary" variant="dark" expand="lg" expanded={expanded}>
 				<Container>
-					<Navbar.Brand as={Link} to="/">
+					<Navbar.Brand as={Link} to="/" onClick={closeNav}>
 						🎓 TNEA Analytics
 					</Navbar.Brand>
-					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Toggle aria-controls="basic-navbar-nav" onClick={navToggle} />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="me-auto">
-							<Nav.Link as={NavLink} to="/search/cutoff">
+							<Nav.Link as={NavLink} to="/search/cutoff" onClick={closeNav}>
 								Cut-off data
 							</Nav.Link>
-							<Nav.Link as={NavLink} to="/search/ranking">
+							<Nav.Link as={NavLink} to="/search/ranking" onClick={closeNav}>
 								Ranking data
 							</Nav.Link>
-							<Nav.Link as={NavLink} to="/search/analytics">
+							<Nav.Link as={NavLink} to="/search/analytics" onClick={closeNav}>
 								Analytics
 							</Nav.Link>
 						</Nav>
